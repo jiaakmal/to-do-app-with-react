8774ef3460f7e26433785f64e2fd51a9824f71c9
jiaakmal <160829841+jiaakmal@users.noreply.github.com>
2025-01-13 14:32:28 +0500

    update appfile

--- to-do-app-with-react/src/components/EditTask.jsx
+++ to-do-app-with-react/src/components/EditTask.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
-// import { useParams, useNavigate } from 'react-router-dom';
 
 const EditTask = ({ task, index, editTask }) => {
   const [newTitle, setNewTitle] = useState(task.title);
   const [newCategory, setNewCategory] = useState(task.category);
   const [newDescription, setNewDescription] = useState(task.description);
-//   const navigate = useNavigate();
+
 
   const handleEdit = () => {
     editTask(index, newTitle, newCategory, newDescription);
   };
 
   return (
-    <div>
+    <div className='taskItem'>
       <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} placeholder="Edit title" />
       <input type="text" value={newCategory} onChange={(e) => setNewCategory(e.target.value)} placeholder="Edit category" />
       <textarea type="text" value={newDescription} onChange={(e) => setNewDescription(e.target.value)} placeholder="Edit description" />
